11998e24baf6300ecdbc263df13843b763370d8f
Myron R Schippers Jr <dubtwee@gmail.com>
2022-07-17 17:30:00 -0500

    stubbing in a admin post edit page to use a url slug

---
+++ components/pages/admin/AdminPostEdit.tsx
@@ -0,0 +1,7 @@
+export default function AdminPostEdit() {
+  return (
+    <main>
+      <h2>Admin Post Edit Page</h2>
+    </main>
+  );
+}
---
+++ pages/admin/[slug].tsx
@@ -0,0 +1,5 @@
+import AdminPostEdit from '../../components/pages/admin/AdminPostEdit';
+
+export default function AdminPostEditPage() {
+  return <AdminPostEdit />;
+}
