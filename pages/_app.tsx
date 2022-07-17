91fb9451a8b026e4e75da5b16701d3c3f38a40eb
Myron R Schippers Jr <dubtwee@gmail.com>
2022-07-17 18:02:44 -0500

    adding a dependency for toast messaging in the app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
-import Navbar from '../components/navbar/Navbar';
+import { Toaster } from 'react-hot-toast';
 import '../styles/globals.css';
+// COMPONENTS
+import Navbar from '../components/navbar/Navbar';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navbar />
       <Component {...pageProps} />
+      <Toaster />
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import toast from 'react-hot-toast';
 import styles from '../styles/Home.module.css';
 // COMPONENTS
 import Loader from '../components/loader/Loader';
@@ -8,6 +9,8 @@ export default function Home() {
       <h1>
         Welcome to <a href="https://nextjs.org">Next.js!</a>
       </h1>
+
+      <button onClick={() => toast.success('hello toast!')}>Toast Me</button>
 
       <Loader show />
     </div>
