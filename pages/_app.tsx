184285256aec16e083c0959d776d0b382aec6b20
Myron R Schippers Jr <dubtwee@gmail.com>
2022-07-17 18:42:19 -0500

    adding a global user context to the Next App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,16 @@
 import { Toaster } from 'react-hot-toast';
+import { UserContext } from '../lib/context';
 import '../styles/globals.css';
 // COMPONENTS
 import Navbar from '../components/navbar/Navbar';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <UserContext.Provider value={{ user: {}, username: 'Shep' }}>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
-    </>
+    </UserContext.Provider>
   );
 }
 
