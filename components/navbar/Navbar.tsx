12f1de6c90a94026f62bd1cedda5d5acc7b87609
Myron R Schippers Jr <dubtwee@gmail.com>
2022-07-17 17:56:33 -0500

    creating Navbar component content and accounting for a logged in and logged out state difference

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,6 +1,45 @@
-// React Component for the Navbar
+import Link from 'next/link';
+
 function Navbar() {
-  return <div>Navigation</div>;
+  const user = null;
+  const username = null;
+
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link href="/">
+            <button className="btn-logo">FEED</button>
+          </Link>
+        </li>
+
+        {/* user is signed-in and has username */}
+        {username && (
+          <>
+            <li>
+              <Link href="/admin">
+                <button className="btn-blue">Write Posts</button>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/${username}`}>
+                <img src={user?.photoURL} />
+              </Link>
+            </li>
+          </>
+        )}
+
+        {/* user is NOT signed-in or has not created a username */}
+        {!username && (
+          <li>
+            <Link href="/enter">
+              <button className="btn-blue">Log in</button>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
