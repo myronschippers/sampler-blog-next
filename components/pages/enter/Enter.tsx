afba23b984b0cd453d7a06b3b3f1d5f246db3c1d
Myron R Schippers Jr <dubtwee@gmail.com>
2022-07-17 18:37:44 -0500

    setting up initial google sign in

--- components/pages/enter/Enter.tsx
+++ components/pages/enter/Enter.tsx
@@ -1,7 +1,47 @@
+import {
+  auth,
+  googleAuthProvider,
+  authSignInWithPopup,
+} from '../../../lib/firebase';
+
 export default function Enter() {
+  const user = null;
+  const username = null;
+
   return (
     <main>
-      <h2>Enter Page</h2>
+      <h2>Sign in</h2>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
     </main>
   );
+}
+
+// Sign in with Google button
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    await authSignInWithPopup(auth, googleAuthProvider);
+  };
+
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src={'/google-logo.png'} /> Sign in with Google
+    </button>
+  );
+}
+
+// Sign out button
+function SignOutButton() {
+  return <button onClick={() => auth.signOut()}>Sign out</button>;
+}
+
+function UsernameForm() {
+  return null;
 }
--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,10 +1,9 @@
 // For Firebase Configurations
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getAuth } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
@@ -22,8 +21,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
+export const googleAuthProvider = new GoogleAuthProvider();
+export const authSignInWithPopup = signInWithPopup;
 export const firestore = getFirestore(app);
 export const storage = getStorage(app);
