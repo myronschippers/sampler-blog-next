18e91f07a9bbf3741c539fa5c1411916b754defc
Myron R Schippers Jr <dubtwee@gmail.com>
2022-07-17 17:41:55 -0500

    adding spinning loard component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import styles from '../styles/Home.module.css';
+// COMPONENTS
+import Loader from '../components/loader/Loader';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <main>
-        <h1>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
+      <h1>
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </h1>
+
+      <Loader show />
     </div>
   );
 }
