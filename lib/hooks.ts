e831ccc3e107b966e0dbff50041d8a7737a10dbe
Myron R Schippers Jr <dubtwee@gmail.com>
2022-07-17 23:02:47 -0500

    cleaning up the useUserData custom hook

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -1,23 +1,18 @@
 import { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import {
-  auth,
-  firestoreDb,
-  firestoreDoc,
-  dbUserSnapshot,
-} from '../lib/firebase';
+import { auth, dbUserSnapshot } from '../lib/firebase';
 
 // Custom hook to read auth record and user profile doc
 export function useUserData() {
   const [user] = useAuthState(auth);
   const [username, setUsername] = useState(null);
+  console.log('useUserData hook - user', user);
 
   useEffect(() => {
     // turn off realtime subscription
     let unsubscribe;
 
     if (user) {
-      const ref = firestoreDoc(firestoreDb, user.uid);
       unsubscribe = dbUserSnapshot(user.uid, (userDoc) => {
         setUsername(userDoc.data()?.username);
       });
